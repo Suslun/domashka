b2793816f7d5aeabc826db0b46b7892a6a07e81d
ПК-ПК\ПК <NikGolun@gmail.com>
2018-10-20 23:34:51 +0300

    Bag1
    Signed-off-by: ПК-ПК\ПК <NikGolun@gmail.com>

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿// School of the young programmer
+using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
